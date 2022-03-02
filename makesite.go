b128d43f324a1d4aa8664ff272b8c3fc01174adc
Hani J <hanijandali@gmail.com>
2022-03-02 13:08:54 -0800

    Addition of first post html template rendered from first post text

--- makesite.go
+++ makesite.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"io/ioutil"
+	"os"
+	"text/template"
+)
+
+type Page struct {
+	//HTMLPagePath and Content of type string
+	HTMLPagePath string
+	Content      string
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	// use ioutil library with built in function of ReadFile to read
+	// create error statement to abort program if problem encountered
+	fileContents, err := ioutil.ReadFile("first-post.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	page := Page{
+		HTMLPagePath: "first-post.html",
+		Content:      string(fileContents),
+	}
+
+	//Create the template to render into our new HTML file
+	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+
+	// Call in HTMLPagePath to show what page to create
+	newFile, err := os.Create(page.HTMLPagePath)
+	if err != nil {
+		panic(err)
+	}
+
+	t.Execute(newFile, page)
 }
