ce13766c270ad16a044c61662919ffdbcc35b6f7
Hani J <hanijandali@gmail.com>
2022-03-02 14:47:12 -0800

    Addition of flag to generate html page from input file

--- makesite.go
+++ makesite.go
@@ -1,38 +1,44 @@
 package main
 
 import (
+	"flag"
 	"io/ioutil"
 	"os"
+	"strings"
 	"text/template"
 )
 
 type Page struct {
-	//HTMLPagePath and Content of type string
-	HTMLPagePath string
-	Content      string
+	// Content of type string
+	Content string
 }
 
 func main() {
-	// use ioutil library with built in function of ReadFile to read
-	// create error statement to abort program if problem encountered
-	fileContents, err := ioutil.ReadFile("first-post.txt")
-	if err != nil {
-		panic(err)
-	}
 
+	fileParse := flag.String("file", "", "The file to read in")
+	flag.Parse()
+
+	//Have file as string
+	linkHTML := strings.Split(*fileParse, ".")[0] + ".html"
+	//Now linkHTML will be string name of our parsed file stored in index 0 location where pointer is looking
+
+	// use ioutil library with built in function of ReadFile to read
+	fileContents, _ := ioutil.ReadFile(*fileParse)
+
+	// Struct to display contents of our page
 	page := Page{
-		HTMLPagePath: "first-post.html",
-		Content:      string(fileContents),
+		Content: string(fileContents),
 	}
 
 	//Create the template to render into our new HTML file
-	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+	t, _ := template.ParseFiles("template.tmpl")
 
-	// Call in HTMLPagePath to show what page to create
-	newFile, err := os.Create(page.HTMLPagePath)
+	// Call in linkHTML to create the new file
+	newFile, _ := os.Create(linkHTML)
+
+	err := t.Execute(newFile, page)
+
 	if err != nil {
 		panic(err)
 	}
-
-	t.Execute(newFile, page)
 }
